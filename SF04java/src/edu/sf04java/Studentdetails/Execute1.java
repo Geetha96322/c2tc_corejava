2a854605f87eecad2fb9b72f2aabe8f073edfff9
DELL <DELL@DESKTOP-139U9HI>
2024-06-11 21:45:36 +0530

    second commit

--- SF04java/src/edu/sf04java/Studentdetails/Execute1.java
+++ SF04java/src/edu/sf04java/Studentdetails/Execute1.java
@@ -1,5 +1,16 @@
 package edu.sf04java.Studentdetails;
-
+import java.util.Base;
 public class Execute1 {
+	public static void main(String[] args) {
+		Base b2=new Base();
+		System.out.println(b2.variableProtected);
+		System.out.println(b2.variableprivate); 
+		System.out.println(b2.variableDefault);
+		System.out.println(b2.variablepublic);
+		
+		
+		
+		
+	}
 
 }
